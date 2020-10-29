ffbb19726f7d4858aaba1e5d07e19a47ba4b3aa2
Kahina <38386505+Kahinator@users.noreply.github.com>
2020-10-29 18:40:00 +0000

    Update CDisappearPlatform.cpp
    Added some comments

--- Classes/GamerCamp/GameSpecific/ManicMiner/Platform/CDisappearPlatform.cpp
+++ Classes/GamerCamp/GameSpecific/ManicMiner/Platform/CDisappearPlatform.cpp
@@ -2,6 +2,7 @@
 #include "CDisappearPlatform.h"
 #endif
 
+// Include Files
 #include "GamerCamp/GCCocosInterface/GCCocosHelpers.h"
 #include "GamerCamp/GCObject/GCObjectManager.h"
 #include "GamerCamp/GCCocosInterface/IGCGameLayer.h"
@@ -9,11 +10,11 @@
 
 USING_NS_CC;
 
-//////////////////////////////////////////////////////////////////////////
-// 
-//////////////////////////////////////////////////////////////////////////
+// Macro
+// Sprite Object implementation
 IN_CPP_CREATION_PARAMS_DECLARE(CDisappearPlatform, "TexturePacker/Sprites/Platforms/Blue/CC_Platform_Regular.plist", "platformblue", b2_staticBody, true);
-//virtual 
+
+// Virtual Function
 void CDisappearPlatform::VOnResourceAcquire(void)
 {
 	IN_CPP_CREATION_PARAMS_AT_TOP_OF_VONRESOURCEACQUIRE(CDisappearPlatform);
